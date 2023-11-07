e7f2710e50f4c13a2fdc240c4d469cafbdc80d9c
ldvalle3 <127244361+ldvalle3@users.noreply.github.com>
2023-11-07 15:16:45 -0700

    Lissette (#2)
    * added movie class
    
    * added movie class

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,25 @@
 // import the Media class:
-
+const Media = require('./Media');
 // create your Movie class:
+class Movie extends Media{
+    constructor(title, genre, year, director, duration, rating){
+        super(title, genre, year);
+        this.director = director;
+        this.duration = duration;
+        this.rating = rating;
+    }
+
+    summary(){
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Duration: ${this.genre}, Rating: ${this.rating} `
+    }
+
+    static longestMovie(movies){
+        return movies.reduce((longest, currentMovie) => {
+            return currentMovie.duration > longest.duration ? currentMovie : longest;
+          });
+    }
+    
+}
 
 // don't change below
 module.exports = Movie;
