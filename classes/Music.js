46c4a6eb060f8a9add365e8c097da9d911bd1155
Kevin Dutzy <35307720+Keffdu@users.noreply.github.com>
2023-11-07 15:33:30 -0700

    added Music class and edited Movie class (#3)

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,30 @@
 // import the Media class:
-
+const Media = require("./Media")
 // create your Music class:
+class Music extends Media {
+    constructor(title, genre, year, artist, length){
+
+    super(title, genre, year)
+
+    this.artist = artist
+    this.length = length
+}
+
+static shortestAlbum(music) {
+    let shortest = music[0]
+    music.map((song) => {
+        if (song.length < shortest.length) {
+            shortest = song
+        }
+    })
+    return shortest
+}
+
+    summary() {
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+}
+
 
 // don't change below
 module.exports = Music;
